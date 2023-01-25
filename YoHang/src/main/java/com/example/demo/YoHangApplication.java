0ba71f4048ad5a5972421292357cfb4572dbd4f6
Lenovo <Lenovo@DESKTOP-8K78OEJ>
2023-01-25 16:23:20 +0900

    test1

---
+++ YoHang/src/main/java/com/example/demo/YoHangApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoHangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoHangApplication.class, args);
+	}
+
+}
