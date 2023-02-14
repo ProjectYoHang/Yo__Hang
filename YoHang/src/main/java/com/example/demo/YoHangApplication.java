000ead9e529772033ddd6f9068f9531a345e1011
Jeong <wjddnr0709@gmail.com>
2023-02-15 08:59:03 +0900

    아이디찾기 수정

--- YoHang/src/main/java/com/example/demo/YoHangApplication.java
+++ YoHang/src/main/java/com/example/demo/YoHangApplication.java
@@ -10,7 +10,13 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example.demo", "com.example.controller", "com.example.model", "com.example.config"})
 @MapperScan(basePackages = {"com.example.mapper"})
-public class YoHangApplication {
+public class YoHangApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(YoHangApplication.class, args);
