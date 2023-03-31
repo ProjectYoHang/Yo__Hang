8cf175f128aea99e3a3d7be043d68ebe1aa5873d
sohn <sonk1023@naver.com>
2023-03-31 13:57:06 +0900

    웹소켓 수정 중....

--- YoHang/src/main/java/com/example/config/WebSocketConfig.java
+++ YoHang/src/main/java/com/example/config/WebSocketConfig.java
@@ -2,16 +2,35 @@ package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 
 //@Component
 @Configuration
+//@EnableWebSocket
 public class WebSocketConfig {
+//public class WebSocketConfig implements WebSocketConfigurer {
+
+	//@Override
+	//public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+	//	registry.addHandler(myHandler(), "/room");
+	//}
+
+	//@Bean
+	//public WebSocketHandler myHandler() {
+	//	return new MyHandler();
+	//}
+	
 	
 	@Bean
 	public ServerEndpointExporter serverEndpointExporter() {
 		return new ServerEndpointExporter();
 	}
+	
+
 	
 }
